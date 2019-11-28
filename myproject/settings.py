f87a7d031d85f2451a35780e11846717f48b06a8
brujeshagrawal <brujesha@mindfiresolutions.com>
2019-11-28 11:44:30 +0530

    Debaug False

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '1#))yxjt1x&(_nw9nak=hj^cx129mpczcem$9-mq%(1-9o*3uq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
